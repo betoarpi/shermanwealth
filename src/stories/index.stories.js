04572d141e2cd34028d1ffa70ad89a717f5608cd
Rob <betoarpi@gmail.com>
2019-10-09 13:52:41 -0500

    Added storybook

---
+++ src/stories/index.stories.js
@@ -0,0 +1,9 @@
+export default { title: 'Button' };
+
+export const btnLinkPrimary = () => '<a href="/" class="btn btn--primary">Button Primary</a>';
+export const btnLinkLight = () => '<a href="/" class="btn btn--light">Button Light</a>';
+export const btnLinkCTA = () => '<a href="/" class="btn btn--cta">Button CTA</a>';
+
+export const btnPrimary = () => '<button type="button" class="btn btn--primary">Button Primary</button>';
+export const btnLight = () => '<button type="button" class="btn btn--light">Button Light</button>';
+export const btnCTA = () => '<button type="button" class="btn btn--cta">Button CTA</button>';
