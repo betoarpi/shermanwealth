311087dd302fa97dda4480904cf4504340d5fc0f
Rob <betoarpi@gmail.com>
2020-02-01 21:41:08 -0600

    Modified the number of posts in search pagination and removed the thumbnail where it doesn't have any

--- frontend/src/components/SearchContainer/index.js
+++ frontend/src/components/SearchContainer/index.js
@@ -11,7 +11,7 @@ export default class SearchContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      renderPerPage: 6,
+      renderPerPage: 33,
       data: [],
       pageCount: 0,
       isLoaded: false
@@ -19,17 +19,17 @@ export default class SearchContainer extends Component {
   }
 
   componentDidUpdate() {
-      const indexOfLastRender = 1 * this.state.renderPerPage
-      const indexOfFirstRender = indexOfLastRender - this.state.renderPerPage
-      const currentRender = this.props.content.slice(indexOfFirstRender, indexOfLastRender)
+    const indexOfLastRender = 1 * this.state.renderPerPage
+    const indexOfFirstRender = indexOfLastRender - this.state.renderPerPage
+    const currentRender = this.props.content.slice(indexOfFirstRender, indexOfLastRender)
 
-      if (!this.state.isLoaded) {
-        this.setState({
-          data: currentRender,
-          isLoaded: true,
-          pageCount: Math.ceil(this.props.content.length / this.state.renderPerPage),
-        })
-      }
+    if (!this.state.isLoaded) {
+      this.setState({
+        data: currentRender,
+        isLoaded: true,
+        pageCount: Math.ceil(this.props.content.length / this.state.renderPerPage),
+      })
+    }
   }
 
   handlePageClick = (data) => {
@@ -44,7 +44,7 @@ export default class SearchContainer extends Component {
   }
 
   render() {
-    if (this.state.data.length === 0){
+    if (this.state.data.length === 0) {
       return null
     }
     return (
@@ -74,16 +74,14 @@ export default class SearchContainer extends Component {
                       More
                       <FaChevronRight />
                     </Link>
-                    <figure>
-                      {
-                        this.props.withImages
-                          ?
-                          item.featured_media !== null ?
-                            <Img fluid={item.featured_media.localFile.childImageSharp.fluid} alt={item.title} /> :
-                            ''
-                          : ''
-                      }
-                    </figure>
+                    {this.props.withImages ?
+                      item.featured_media !== null ?
+                        <figure>
+                          <Img fluid={item.featured_media.localFile.childImageSharp.fluid} alt={item.title} />
+                        </figure> :
+                        ''
+                      : ''
+                    }
                   </NewsItem>
                 )
               })
@@ -92,20 +90,20 @@ export default class SearchContainer extends Component {
           {
             this.state.pageCount > 1
               ? <PaginationGrid>
-                  <ReactPaginate
-                    previousLabel={'previous'}
-                    nextLabel={'next'}
-                    breakLabel={'...'}
-                    breakClassName={'break-me'}
-                    pageCount={this.state.pageCount}
-                    marginPagesDisplayed={6}
-                    pageRangeDisplayed={6}
-                    onPageChange={this.handlePageClick}
-                    containerClassName={'pagination'}
-                    subContainerClassName={'pages pagination'}
-                    activeClassName={'active'}
-                  />
-                </PaginationGrid>
+                <ReactPaginate
+                  previousLabel={'previous'}
+                  nextLabel={'next'}
+                  breakLabel={'...'}
+                  breakClassName={'break-me'}
+                  pageCount={this.state.pageCount}
+                  marginPagesDisplayed={6}
+                  pageRangeDisplayed={6}
+                  onPageChange={this.handlePageClick}
+                  containerClassName={'pagination'}
+                  subContainerClassName={'pages pagination'}
+                  activeClassName={'active'}
+                />
+              </PaginationGrid>
               : null
           }
         </section>
